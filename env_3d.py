9d1b24b836035db8a7d873bfb726391d9f6ab3b5
ravishende <ravi.sameer.shende@gmail.com>
2025-11-23 09:48:36 -0800

    env_3d: fixed premature ending on win

--- env_3d.py
+++ env_3d.py
@@ -206,10 +206,11 @@ def visualize(game_map:list[np.ndarray], player_locations:list[tuple[int,int]]):
         # Ideally it'd be between 0 and 1, but only needs to be accurate between 1/4 and action_change
         section_progress = (map_distance_travelled - step*(current_slice+1)) / step
 
-        if current_slice == len(player_locations) -1 and section_progress >= 0:
-            # player hit an obstacle --> reset
-            lane_x_position = lane_x_start
-            continue
+        if len(player_locations) < len(slices):  # player didn't win - lost early
+            if current_slice == len(player_locations) -1 and section_progress >= 0:
+                # player hit an obstacle --> reset
+                lane_x_position = lane_x_start
+                continue
 
         if map_distance_travelled < action_change_distance:
             # start the game standing at (1,1) - in the middle of the 3x3 board
